ccd08b2ed6431941d36d93cea60e8b0f8bae9594
TuanTheWall <thaophuongho31@gmail.com>
2025-12-26 13:58:57 +0700

    Delete assets directory

--- assets/js/app.ts
+++
@@ -1,8 +0,0 @@
-const menuHamburger: HTMLElement | null = document.querySelector(".menu-btn");
-const navLinks: HTMLElement | null = document.querySelector(".nav-links");
-
-if (menuHamburger && navLinks) {
-    menuHamburger.addEventListener("click", () => {
-        navLinks.classList.toggle('mobile-menu');
-    });
-}
